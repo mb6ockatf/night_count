c81b74f63dbcddca4b24d34733535235b7333aef
BigProject404 <bigproject404@yandex.ru>
2022-02-18 19:21:17 +0300

    Create skeleton

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import pymysql
+import mysql.connector
+
+dbconfig = {'host': '127.0.0.1',
+            'user': 'root',
+            'password': 'root',
+            'database': 'sakila'
+            }
 
 try:
-    print(pymysql)
+    connection = mysql.connector.connect(**dbconfig)
+    with connection as conn:
+        cursor = conn.cursor()
+        cursor.execute("select version()")
+        version = cursor.fetchone()
+        print(f"Database version: {version}")
 except Exception as ex:
     print('It went out of our control')
     print(ex)
